8e2e38b2a0455aa402f55dacfb2aab1afce2750b
LouisChristopher-001 <louischristopher784@gmail.com>
2024-09-07 14:34:26 +0530

    Update useAuth.js

--- frontend/src/hooks/useAuth.js
+++ frontend/src/hooks/useAuth.js
@@ -3,12 +3,12 @@ import { useNavigate } from 'react-router-dom';
 import Cookies from 'js-cookie';
 
 export const useAuth = () => {
-  const navigate = useNavigate();
-  useEffect(() => {
-    const token = Cookies.get('token'); // Ensure this matches the cookie name set in the backend
-    if (!token) {
-      console.log('No token found, redirecting to login');
-      navigate('/'); // Redirect to login page if token is not found
+  // const navigate = useNavigate();
+  // useEffect(() => {
+  //   const token = Cookies.get('token'); // Ensure this matches the cookie name set in the backend
+  //   if (!token) {
+  //     console.log('No token found, redirecting to login');
+  //     navigate('/'); // Redirect to login page if token is not found
     }
   }, [navigate]);
 };
